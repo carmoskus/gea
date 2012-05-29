ce216d0dea3e9a9c0374604b9562b3dcf7fc4ced
Chris Armoskus <carmoskus@gmail.com>
2012-05-28 18:58:15 -0700

    Skinned admin. Added first views & templates.

--- qpcr/urls.py
+++ qpcr/urls.py
@@ -8,11 +8,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gea.views.home', name='home'),
     # url(r'^gea/', include('gea.foo.urls')),
-	url(r'^qpcr/', include('qpcr.urls')),
-	
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^new/', 'qpcr.views.newRun'),
 )
--- qpcr/views.py
+++ qpcr/views.py
@@ -1,1 +1,14 @@
+from django.template import Context, loader
+from django.http import HttpResponse
+
 # Create your views here.
+
+#def newRun(request):
+#	return HttpResponse("Uploading a new run")
+	
+def newRun(request):
+	t = loader.get_template('qpcr/new_run.html')
+	c = Context({
+	})
+	
+	return HttpResponse(t.render(c))
